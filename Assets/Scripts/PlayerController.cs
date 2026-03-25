2d6adffa0d04ed4e42f75add4f531f50c6113de6
Jimothy <teen_gamer2007@yahoo.com>
2026-03-24 20:45:08 -0700

    Ground Check
    Added a ground check and linked the jump to it

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private int health = 3;
-    [SerializeField] private float jumpHeight = 5f;
+    [SerializeField] private float jumpHeight = 7f;
     [SerializeField] private bool isGrounded;
     [SerializeField] private bool jumpPressed;
+    [SerializeField] private BoxCollider2D groundCheck;
+
     private Rigidbody2D rb;
     private UnityEngine.InputSystem.PlayerInput controls;
 
@@ -20,20 +22,37 @@ public class PlayerController : MonoBehaviour
         {
             controls = GetComponent<UnityEngine.InputSystem.PlayerInput>();
         }
+        if (groundCheck == null)
+        {
+            Debug.Log("Ground Check Collider is not assigned in the inspector.");
+        }
     }
 
     void FixedUpdate()
     {
         if (isGrounded && jumpPressed)
         {
-            Debug.Log("Jumping");
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpHeight);
+            isGrounded = false;
             jumpPressed = false;
+        }
+        else if (jumpPressed)
+        {
+            jumpPressed = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
         }
     }
 
     void OnJump(InputValue value)
     {
+        Debug.Log("Jump Pressed");
         jumpPressed = value.isPressed;
     }
 }
