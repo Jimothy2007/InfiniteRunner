3319d4ca51a14d50fafd40cce70c17d2ce49a9ca
Jimothy <teen_gamer2007@yahoo.com>
2026-03-24 20:18:27 -0700

    Jump
    Added a jump that will work when pressing w or up arrow

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,28 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpHeight = 5f;
     [SerializeField] private bool isGrounded;
     [SerializeField] private bool jumpPressed;
-    [SerializeField] private Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private UnityEngine.InputSystem.PlayerInput controls;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+        if (controls == null)
+        {
+            controls = GetComponent<UnityEngine.InputSystem.PlayerInput>();
+        }
     }
 
     void FixedUpdate()
     {
         if (isGrounded && jumpPressed)
         {
+            Debug.Log("Jumping");
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpHeight);
+            jumpPressed = false;
         }
     }
 
